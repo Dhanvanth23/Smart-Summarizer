1a281b4aa8f2cb6c3084ca6ccfe2950479369dd2
Your Name <your-email@example.com>
2025-04-02 10:39:06 +0530

    Updated changes

--- app.py
+++ app.py
@@ -284,4 +284,4 @@ def history():
     return render_template('history.html', languages=SUPPORTED_LANGUAGES, categories=NEWS_CATEGORIES)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)   
